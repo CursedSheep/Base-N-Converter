d0b35c98d00b56ae07993d9527dfd12d26b1eecf
CursedSheep <tyronederp16@gmail.com>
2021-10-18 21:06:54 +0800

    Add files via upload

---
+++ BaseNConverter/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace BaseNConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FormulaTxt = new System.Windows.Forms.RichTextBox();
+            this.SecondValue = new System.Windows.Forms.TextBox();
+            this.FirstValue = new System.Windows.Forms.TextBox();
+            this.SwapValue = new System.Windows.Forms.Button();
+            this.secondBaseN = new System.Windows.Forms.NumericUpDown();
+            this.firstBaseN = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.secondBaseN)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.firstBaseN)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FormulaTxt
+            // 
+            this.FormulaTxt.Location = new System.Drawing.Point(12, 67);
+            this.FormulaTxt.Name = "FormulaTxt";
+            this.FormulaTxt.Size = new System.Drawing.Size(243, 221);
+            this.FormulaTxt.TabIndex = 3;
+            this.FormulaTxt.Text = "";
+            // 
+            // SecondValue
+            // 
+            this.SecondValue.Location = new System.Drawing.Point(154, 13);
+            this.SecondValue.Name = "SecondValue";
+            this.SecondValue.Size = new System.Drawing.Size(100, 20);
+            this.SecondValue.TabIndex = 8;
+            // 
+            // FirstValue
+            // 
+            this.FirstValue.Location = new System.Drawing.Point(11, 13);
+            this.FirstValue.Name = "FirstValue";
+            this.FirstValue.Size = new System.Drawing.Size(100, 20);
+            this.FirstValue.TabIndex = 7;
+            this.FirstValue.TextChanged += new System.EventHandler(this.FirstValue_TextChanged);
+            // 
+            // SwapValue
+            // 
+            this.SwapValue.Location = new System.Drawing.Point(117, 26);
+            this.SwapValue.Name = "SwapValue";
+            this.SwapValue.Size = new System.Drawing.Size(31, 23);
+            this.SwapValue.TabIndex = 6;
+            this.SwapValue.Text = "<->";
+            this.SwapValue.UseVisualStyleBackColor = true;
+            this.SwapValue.Click += new System.EventHandler(this.SwapValue_Click);
+            // 
+            // secondBaseN
+            // 
+            this.secondBaseN.Location = new System.Drawing.Point(154, 39);
+            this.secondBaseN.Name = "secondBaseN";
+            this.secondBaseN.Size = new System.Drawing.Size(100, 20);
+            this.secondBaseN.TabIndex = 10;
+            this.secondBaseN.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.secondBaseN.ValueChanged += new System.EventHandler(this.secondBaseN_ValueChanged);
+            // 
+            // firstBaseN
+            // 
+            this.firstBaseN.Location = new System.Drawing.Point(11, 39);
+            this.firstBaseN.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.firstBaseN.Name = "firstBaseN";
+            this.firstBaseN.Size = new System.Drawing.Size(100, 20);
+            this.firstBaseN.TabIndex = 11;
+            this.firstBaseN.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.firstBaseN.ValueChanged += new System.EventHandler(this.firstBaseN_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 302);
+            this.Controls.Add(this.firstBaseN);
+            this.Controls.Add(this.secondBaseN);
+            this.Controls.Add(this.SecondValue);
+            this.Controls.Add(this.FirstValue);
+            this.Controls.Add(this.SwapValue);
+            this.Controls.Add(this.FormulaTxt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "BaseN Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.secondBaseN)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.firstBaseN)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox FormulaTxt;
+        private System.Windows.Forms.TextBox SecondValue;
+        private System.Windows.Forms.TextBox FirstValue;
+        private System.Windows.Forms.Button SwapValue;
+        private System.Windows.Forms.NumericUpDown secondBaseN;
+        private System.Windows.Forms.NumericUpDown firstBaseN;
+    }
+}
+
---
+++ BaseNConverter/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BaseNConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            FormulaTxt.ReadOnly = true;
+            SecondValue.ReadOnly = true;
+        }
+        private static string DecimalToBaseN(int value, int n, StringBuilder formula)
+        {
+            int num = value;
+            int basevalue = n;
+            StringBuilder sb = new StringBuilder();
+            List<int> characterList = new List<int>();
+            formula.Append($"Divide {value} to {n} until quotient is 0: \n");
+            while (num > 0)
+            {
+                var q = num / basevalue;
+                var r = num - (q * basevalue);
+                formula.Append($"{num}/{basevalue} = {q}.{r}\n");
+                characterList.Insert(0, r);
+                sb.Insert(0, (char)((r > 9 ? '7' : '0') + r));
+                num = q;
+            }
+
+            formula.Append('\n');
+            formula.Append("Read the remainders from bottom to top to get the value: \n");
+            formula.Append("result => ");
+            formula.Append(string.Join(" ", characterList.Select(x => x.ToString()).ToArray()) + "\n\n");
+            if(characterList.Exists(x => x > 9))
+            {
+                formula.Append("Convert numbers more than 9 to the character code next to it: \n");
+                formula.Append("result => ");
+                formula.Append(string.Join(" ", characterList.Select(x => ((char)((x > 9 ? '7' : '0') + x)).ToString()).ToArray()) + '\n');
+            }
+            formula.Append("result => ");
+            formula.Append(sb.ToString());
+            return sb.ToString();
+        }
+        private static int BaseNToDecimal(string str, int n, StringBuilder formula)
+        {
+            List<int> characterList = new List<int>();
+            int num = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                var currChar = str[i];
+                int ConvertedNum = currChar - (char.IsDigit(currChar) ? '0' : 55);
+                characterList.Add(ConvertedNum);
+                num += ConvertedNum * (int)Math.Pow(n, str.Length - (i + 1));
+            }
+            if(characterList.Exists(x => x > 9))
+            {
+                formula.Append("Convert letters back to integer: \n");
+                formula.Append("result => ");
+                formula.Append(string.Join(" ", characterList.Select(x => x.ToString()).ToArray()) + '\n');
+            }
+            formula.Append($"Multiply each digit by ({n}^(n-1)) where n is the digit length: \n");
+            List<string> tmpstr = new List<string>();
+            for(int i = 0; i < characterList.Count; i++)
+                tmpstr.Add($"({characterList[i]}*({n}^({characterList.Count - (i + 1)})))");
+
+            formula.Append("result => ");
+            formula.Append(string.Join(" + ", tmpstr.ToArray()) + '\n');
+            tmpstr.Clear();
+
+            for (int i = 0; i < characterList.Count; i++)
+                tmpstr.Add($"({characterList[i]}*{Math.Pow(n, characterList.Count - (i + 1))})");
+
+            formula.Append("result => ");
+            formula.Append(string.Join(" + ", tmpstr.ToArray()) + '\n');
+            tmpstr.Clear();
+
+            for (int i = 0; i < characterList.Count; i++)
+                tmpstr.Add($"{characterList[i] * Math.Pow(n, characterList.Count - (i + 1))}");
+
+            formula.Append("result => ");
+            formula.Append(string.Join(" + ", tmpstr.ToArray()) + '\n');
+            tmpstr.Clear();
+
+            formula.Append("result => ");
+            formula.Append(num);
+
+            return num;
+        }
+
+        private bool IsValidBaseN(string chars, int basevalue)
+        {
+            if (chars.Length == 0) return false;
+            bool isValid;
+            foreach (var c in chars)
+            {
+                if (basevalue > 9 && c > '9')
+                {
+                    isValid = c >= 'A' && c <= ('A' + (basevalue - 11));
+                }
+                else
+                    isValid = c >= '0' && c <= ('0' + (basevalue - 1));
+
+                if (!isValid)
+                    return false;
+            }
+            return true;
+        }
+        private void DoMagic()
+        {
+            try
+            {
+                if (IsValidBaseN(FirstValue.Text, (int)firstBaseN.Value))
+                {
+                    StringBuilder formula = new StringBuilder();
+                    if ((int)firstBaseN.Value == 10)
+                        SecondValue.Text = DecimalToBaseN(int.Parse(FirstValue.Text), (int)secondBaseN.Value, formula);
+                    else if ((int)firstBaseN.Value == (int)secondBaseN.Value)
+                    {
+                        formula.Append($"{FirstValue.Text} = {SecondValue.Text} \n");
+                        SecondValue.Text = FirstValue.Text;
+                    }
+                    else
+                    {
+                        formula.Append($"Convert {FirstValue.Text} to decimal first: \n");
+                        int decimalvalue = BaseNToDecimal(FirstValue.Text, (int)firstBaseN.Value, formula);
+                        formula.Append($"\n\nThen we convert {decimalvalue} to Base {secondBaseN.Value}: \n");
+                        SecondValue.Text = DecimalToBaseN(decimalvalue, (int)secondBaseN.Value, formula);
+                    }
+                    FormulaTxt.Text = formula.ToString();
+                }
+                else
+                    FormulaTxt.Text = "Invalid input!";
+            }
+            catch
+            {
+                FormulaTxt.Text = "Invalid input!";
+            }
+        }
+        private void FirstValue_TextChanged(object sender, EventArgs e)
+        {
+            DoMagic();
+        }
+
+        private void SwapValue_Click(object sender, EventArgs e)
+        {
+            string secvalue = SecondValue.Text;
+            SecondValue.Text = FirstValue.Text;
+            FirstValue.Text = secvalue;
+
+            var secondDecimalval = secondBaseN.Value;
+            secondBaseN.Value = firstBaseN.Value;
+            firstBaseN.Value = secondDecimalval;
+
+        }
+
+        private void firstBaseN_ValueChanged(object sender, EventArgs e)
+        {
+            DoMagic();
+        }
+
+        private void secondBaseN_ValueChanged(object sender, EventArgs e)
+        {
+            DoMagic();
+        }
+    }
+}
